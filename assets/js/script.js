1d52880076c5d6efd4aa7c66054d687b48eb18e6
jschrbr <schrbrr@gmail.com>
2020-03-13 23:30:29 +1100

    more cleaning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,62 @@
 const recipeCards = $("#recipes");
 const ingredientsTitle = $("#ingredientsTitle");
 const ingredientsCard = $("#ingredientsCard");
+
 function createEl(tag) {
   return $(`<${tag}>`);
+}
+
+function queryAPI(query, callback) {
+  $.ajax({
+    url: query,
+    method: "GET"
+  }).then(function(response) {
+    callback(response);
+  });
+}
+
+function getIngredients(id, rate) {
+  queryURL =
+    "https://api.spoonacular.com/recipes/" +
+    id +
+    "/priceBreakdownWidget.json?apiKey=a6e7c38ff34b4abfaef92a07685d0635";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(resp) {
+      ingredientsCard.show();
+
+      res = resp.ingredients;
+      $("tbody").text("");
+      res.forEach(ingrd => {
+        let tr = createEl("tr");
+        let tdName = createEl("td").text(ingrd.name);
+        let tdQty = createEl("td").text(
+          `${ingrd.amount.metric.value} ${ingrd.amount.metric.unit}`
+        );
+        let tdPrice = createEl("td").text(
+          `$${((ingrd.price * rate) / 100).toFixed(2)}`
+        );
+        tr.append(tdName, tdQty, tdPrice);
+        $("tbody").append(tr);
+      });
+    })
+    .fail(function(resp) {
+      console.log(resp);
+    });
+}
+
+function getConversionRate(id) {
+  url =
+    "https://free.currconv.com/api/v7/convert?q=USD_AUD&compact=ultra&apiKey=07b4e474a20765fec5ef";
+  $.ajax({
+    url: url,
+    method: "GET"
+  }).then(function(resp) {
+    let rate = resp[Object.keys(resp)];
+    getIngredients(id, rate);
+  });
 }
 
 function renderCards(response) {
@@ -35,29 +89,17 @@ function renderCards(response) {
   $(".card-btn").click(queryIngredients);
 }
 
-function buildQuery(query, callback) {
-  var queryURL = `https://api.spoonacular.com/recipes/search?query=${query}&apiKey=7fd63fa14b66441e9190b97a36f40c22`;
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    callback(response);
-  });
-}
-
-// buildQuery("mash");
-
 function queryRecipe(e) {
   e.preventDefault();
   query = $("#search").val();
   if (query !== "") {
-    buildQuery(query, renderCards);
+    var query = `https://api.spoonacular.com/recipes/search?query=${query}&apiKey=7fd63fa14b66441e9190b97a36f40c22`;
+    queryAPI(query, renderCards);
   }
 }
 
 function queryIngredients() {
-  console.log($(this)[0].id);
+  getConversionRate($(this)[0].id);
 }
 
 ingredientsCard.hide();
