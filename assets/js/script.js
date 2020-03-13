66e904324e9746b93c45e05f4efe547e5ec4bd05
jschrbr <schrbrr@gmail.com>
2020-03-13 22:35:25 +1100

    Fixed some stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,60 @@
-
-
-function buildQuery(){
-    //this is the query URL we will use to query API 
-    // var query = searchQuery;
-
-    // var queryURL = "https://api.spoonacular.com/recipes/search?"+"query=burger"+"&apiKey=7fd63fa14b66441e9190b97a36f40c22";
-    var queryURL = "https://api.spoonacular.com/recipes/search?query=burger&apiKey=7fd63fa14b66441e9190b97a36f40c22"
-    // fetch(queryURL)
-    // .then(function(data){
-    //     console.log(data)
-    // });
-
-    $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function (response){
-    
-        var results = response.results;
-        console.log(results)
-
-        for (index in results){
-            var text_content = "Title: " + results[index].title;
-            var text = document.createTextNode(text_content);
-            var tag_element = $("<div>");
-            tag_element.append(text);
-            var body = $("body")
-            console.log(index)
-            
-            var image_element = $("<img>").attr("src","https://spoonacular.com/recipeImages/"+results[index].imageUrls);
-            // console.log(results[index].imageUrls);
-            body.append(image_element);
-        }        
-    });
+const recipeCards = $("#recipes");
+const ingredientsTitle = $("#ingredientsTitle");
+const ingredientsCard = $("#ingredientsCard");
+function createEl(tag) {
+  return $(`<${tag}>`);
 }
-buildQuery();
+
+function buildQuery(query) {
+  var queryURL = `https://api.spoonacular.com/recipes/search?query=${query}&apiKey=7fd63fa14b66441e9190b97a36f40c22`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    recipeCards.text("");
+    var results = response.results;
+    results.forEach(result => {
+      let recipeID = result.recipeID;
+      let imgUrl = `https://spoonacular.com/recipeImages/${result.image}`;
+      let col = createEl("div").addClass("col s12 m5 l3");
+      let card = createEl("div").addClass("card small");
+      let buttonWrapper = createEl("a")
+        .attr({
+          href: "javascript:;",
+          id: recipeID
+        })
+        .addClass("card-btn");
+      var imgEl = $("<img>").attr({ src: imgUrl });
+      buttonWrapper.append(imgEl);
+      let cardImg = createEl("div")
+        .addClass("card-image")
+        .append(buttonWrapper);
+      let cardContent = createEl("div").addClass("card-content");
+      var cardTitle = createEl("span")
+        .text(result.title)
+        .addClass("card-title");
+      card.append(cardImg, cardTitle, cardContent);
+      col.append(card);
+      recipeCards.append(col);
+    });
+  });
+}
+
+// buildQuery("mash");
+
+function queryRecipe(e) {
+  e.preventDefault();
+  query = $("#search").val();
+  if (query !== "") {
+    buildQuery(query);
+  }
+}
+
+function queryIngredients() {
+  console.log($(this).firstChild);
+}
+
+ingredientsCard.hide();
+$("form").submit(queryRecipe);
+$(".card-btn").click(queryIngredients);
