efcef5adadb4b4da7cdf8ef2779277f5b599ada3
jschrbr <schrbrr@gmail.com>
2020-03-13 14:55:17 +1100

    Added a bunch

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,67 @@
-let queryWooliesURL =
-  "https://api.spoonacular.com/recipes/search?query=Mashed potatoes&apiKey=a6e7c38ff34b4abfaef92a07685d0635";
-
 function createEl(tag) {
   return $("<" + tag + ">");
 }
 
 const carouselSel = $("#recipes");
 
-$(document).ready(function() {
-  $(".sidenav").sidenav();
+function getIngredients(id, rate) {
+  queryURL =
+    "https://api.spoonacular.com/recipes/" +
+    id +
+    "/priceBreakdownWidget.json?apiKey=a6e7c38ff34b4abfaef92a07685d0635";
+  console.log(rate);
   $.ajax({
-    url: queryWooliesURL,
+    url: queryURL,
     method: "GET"
   })
     .then(function(resp) {
-      console.log(resp.results);
+      console.log(resp.ingredients);
+      res = resp.ingredients;
+      $("tbody").text("");
+      res.forEach(ingrd => {
+        console.log(ingrd.name);
+        let tr = createEl("tr");
+        let tdName = createEl("td").text(ingrd.name);
+        let tdQty = createEl("td").text(
+          `${ingrd.amount.metric.value} ${ingrd.amount.metric.unit}`
+        );
+        let tdPrice = createEl("td").text(
+          `$${((ingrd.price * rate) / 100).toFixed(2)}`
+        );
+        tr.append(tdName, tdQty, tdPrice);
+        $("tbody").append(tr);
+      });
+    })
+    .fail(function(resp) {
+      console.log(resp);
+    });
+}
+
+function getConversionRate() {
+  url =
+    "https://free.currconv.com/api/v7/convert?q=USD_AUD&compact=ultra&apiKey=07b4e474a20765fec5ef";
+  $.ajax({
+    url: url,
+    method: "GET"
+  }).then(function(resp) {
+    let rate = resp[Object.keys(resp)];
+    getIngredients(res[0].id, rate);
+  });
+}
+
+function getRecipes(query) {
+  $.ajax({
+    url:
+      "https://api.spoonacular.com/recipes/search?query=" +
+      query +
+      "&apiKey=a6e7c38ff34b4abfaef92a07685d0635",
+    method: "GET"
+  })
+    .then(function(resp) {
       res = resp.results;
       carouselSel.text("");
       for (var i = 0; i < res.length; i++) {
-        let panelEl = createEl("div").addClass(["carousel-item"]);
+        // let panelEl = createEl("div").addClass(["carousel-item"]);
 
         let card = createEl("div").addClass(["card", "small", "carousel-item"]);
         let cardImage = createEl("div").addClass(["card-image"]);
@@ -42,8 +85,26 @@ $(document).ready(function() {
         fullWidth: true,
         indicators: true
       });
+      getConversionRate();
     })
     .fail(function(resp) {
       console.log(resp);
     });
+}
+
+$(document).ready(function() {
+  let query = "Mashed Potatoes";
+  $(".sidenav").sidenav();
+  getRecipes(query);
 });
+
+function newQuery(e) {
+  e.preventDefault();
+
+  let query = $("input").val();
+  if (query !== "") {
+    getRecipes(query);
+  }
+}
+
+$("form").submit(newQuery);
