b9d4d4e48ce7051e5b0e3fd9c96ec90cd283a637
Haylie Goh <h.goh@deakin.edu.au>
2020-03-14 13:45:31 +1100

    inputs ready to be utilised

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 function buildQuery(userInput){
 
   var queryURL = "https://api.spoonacular.com/recipes/search?query=" +userInput+"&apiKey=7fd63fa14b66441e9190b97a36f40c22";
+  // var queryURL = "https://api.spoonacular.com/recipes/search?query=burger&apiKey=7fd63fa14b66441e9190b97a36f40c22"
 
     $.ajax({
         url:queryURL,
@@ -12,20 +13,36 @@ function buildQuery(userInput){
         console.log(results)
 
         for (index in results){
-            var text_content = "Title: " + results[index].title;
-            var text = document.createTextNode(text_content);
-            var tag_element = $("<div>");
-            tag_element.append(text);
-            var body = $("body")
-            console.log(index)
+            // var text_content = "Title: " + results[index].title;
+            // var text = document.createTextNode(text_content);
+            // var tag_element = $("<div>");
+            // tag_element.append(text);
+            // var body = $("body")
+            // console.log(index)
             
-            var image_element = $("<img>").attr("src","https://spoonacular.com/recipeImages/"+results[index].imageUrls);
-            // console.log(results[index].imageUrls);
-            body.append(image_element);
-        }        
+            // var image_element = $("<img>").attr("src","https://spoonacular.com/recipeImages/"+results[index].imageUrls);
+            // // console.log(results[index].imageUrls);
+            // body.append(image_element);
+
+            var recipeId = results[index].id
+            var recipeTitle = results[index].title
+            var recipeImage = "https://spoonacular.com/recipeImages/"+ results[index].imageUrls
+            console.log(recipeId)
+            console.log(recipeImage)
+            console.log(recipeTitle)
+
+
+        } 
+
+
+        
+
     });
 }
 
+
+
+//this allows the user to search up a recipe
 function searchQuery(){
     $("#recipe-search-field").on("keyup",function(event){
         if (event.keyCode == 13){
