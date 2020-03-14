695ce761f570f3d8f13dd10434e3c6d62afa5626
jschrbr <schrbrr@gmail.com>
2020-03-14 16:14:42 +1100

    Added currency select element to the navbar and an onchange event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,101 @@
 var recipeId;
 var recipeTitle;
 var recipeImage;
+let currencySelect = $("select");
 
-function buildQuery(userInput){
+$(document).ready(function() {
+  currencySelect.formSelect();
 
-  var queryURL = "https://api.spoonacular.com/recipes/search?query=" +userInput+"&number=4&apiKey=7fd63fa14b66441e9190b97a36f40c22";
-  // var queryURL = "https://api.spoonacular.com/recipes/search?query=burger&apiKey=7fd63fa14b66441e9190b97a36f40c22"
+  function buildQuery(userInput) {
+    var queryURL =
+      "https://api.spoonacular.com/recipes/search?query=" +
+      userInput +
+      "&number=4&apiKey=7fd63fa14b66441e9190b97a36f40c22";
+    // var queryURL = "https://api.spoonacular.com/recipes/search?query=burger&apiKey=7fd63fa14b66441e9190b97a36f40c22"
 
     $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function (response){
-        var results = response.results;
-        console.log(results)
-        $("#returned-recipes").empty();
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      var results = response.results;
+      console.log(results);
+      $("#returned-recipes").empty();
 
-        for (index in results){
-             recipeId = results[index].id
-             recipeTitle = results[index].title
-             recipeImage = "https://spoonacular.com/recipeImages/"+ results[index].imageUrls
-            console.log(recipeId)
-            console.log(recipeImage)
-            console.log(recipeTitle)
+      for (index in results) {
+        recipeId = results[index].id;
+        recipeTitle = results[index].title;
+        recipeImage =
+          "https://spoonacular.com/recipeImages/" + results[index].imageUrls;
+        console.log(recipeId);
+        console.log(recipeImage);
+        console.log(recipeTitle);
 
-          cardBuilder(recipeId,recipeImage,recipeTitle);
-        } 
+        cardBuilder(recipeId, recipeImage, recipeTitle);
+      }
     });
-}
+  }
 
-//this allows the user to search up a recipe
-function searchQuery(){
-    $("#recipe-search-field").on("keyup",function(event){
-        if (event.keyCode == 13){
-            event.preventDefault()
-            var userInput = $("#recipe-search-field").val().trim()
-            // return userInput
-            console.log(userInput)
-            buildQuery(userInput);
-        }; 
+  //this allows the user to search up a recipe
+  function searchQuery() {
+    $("#recipe-search-field").on("keyup", function(event) {
+      if (event.keyCode == 13) {
+        event.preventDefault();
+        var userInput = $("#recipe-search-field")
+          .val()
+          .trim();
+        // return userInput
+        console.log(userInput);
+        buildQuery(userInput);
+      }
     });
-}
+  }
 
-//preventing enter button to submit globally
-$(document).ready(function() {
-  $(window).keydown(function(event) {
-    if (event.keyCode == 13) {
-      event.preventDefault();
-      return false;
-    }
+  //preventing enter button to submit globally
+  $(document).ready(function() {
+    $(window).keydown(function(event) {
+      if (event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+      }
+    });
+  });
+
+  searchQuery();
+
+  function cardBuilder() {
+    let recipeCard = $("<div>");
+    recipeCard.attr("class", "col s6 m3");
+    let cardContainer = $("<div>");
+    cardContainer.attr("class", "card");
+    let cardImageContainer = $("<div>");
+    cardImageContainer.attr("class", "card-image recipe-image");
+    let cardImage = $("<img>");
+    cardImage.attr("src", recipeImage);
+    let cardButton = $("<a>");
+    cardButton.attr(
+      "class",
+      "btn-floating halfway-fab waves-effect waves-light red"
+    );
+    cardButton.attr("value", recipeId);
+    let cardIcon = $("<i>");
+    cardIcon.attr("class", "material-icons");
+    let cardTitleContainer = $("<div>");
+    cardTitleContainer.attr("class", "card-content flow-text");
+    let cardTitle = $("<span>");
+    cardTitle.attr("class", "card-title");
+    cardTitle.text(recipeTitle);
+
+    recipeCard.append(cardContainer);
+    cardContainer.append(cardImageContainer);
+    cardImageContainer.append(cardImage);
+    cardImageContainer.append(cardButton);
+    cardButton.append(cardIcon);
+    cardContainer.append(cardTitleContainer);
+    cardTitleContainer.append(cardTitle);
+    $("#returned-recipes").append(recipeCard);
+  }
+
+  currencySelect.on("change", function(e) {
+    console.log(e.target.value);
   });
 });
-
-searchQuery();
-
-function cardBuilder() {
-  let recipeCard = $("<div>");
-  recipeCard.attr("class", "col s6 m3");
-  let cardContainer = $("<div>");
-  cardContainer.attr("class", "card");
-  let cardImageContainer = $("<div>");
-  cardImageContainer.attr("class", "card-image recipe-image");
-  let cardImage = $("<img>");
-  cardImage.attr("src", recipeImage);
-  let cardButton = $("<a>");
-  cardButton.attr(
-    "class",
-    "btn-floating halfway-fab waves-effect waves-light red"
-  );
-  cardButton.attr("value", recipeId);
-  let cardIcon = $("<i>");
-  cardIcon.attr("class", "material-icons");
-  let cardTitleContainer = $("<div>");
-  cardTitleContainer.attr("class", "card-content flow-text");
-  let cardTitle = $("<span>");
-  cardTitle.attr("class", "card-title");
-  cardTitle.text(recipeTitle);
-
-  recipeCard.append(cardContainer);
-  cardContainer.append(cardImageContainer);
-  cardImageContainer.append(cardImage);
-  cardImageContainer.append(cardButton);
-  cardButton.append(cardIcon);
-  cardContainer.append(cardTitleContainer);
-  cardTitleContainer.append(cardTitle);
-  $("#returned-recipes").append(recipeCard);
-}
